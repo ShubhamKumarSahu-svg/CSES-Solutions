dcf20ef19265af7f7e77d549b5e1dd7862c4bd32
ShubhamKumarSahu-svg <shubhamisok@gmail.com>
2026-02-01 12:19:49 +0530

    Add CSES problem 1084: Apartments solution

---
+++ CSES_Problems/1084-Apartments/solution.cpp
@@ -0,0 +1,40 @@
+// CSES Problem 1084: Apartments
+// Link: https://cses.fi/problemset/task/1084/
+// Description: There are n applicants and m free apartments. Your task is to distribute the apa...
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long
+#define vi vector<int>
+#define print(x) cout << (x) << endl;
+#define all(x) (x).begin(), (x).end()
+#define input(v, n)                                                            \
+for (int i = 0; i < n; i++)                                                  \
+cin >> v[i]
+#define read3(a, b, c)                                                         \
+int a, b, c;                                                                 \
+cin >> a >> b >> c;
+signed main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    read3(n, m, k);
+    vi a(n), b(m);
+    input(a, n);
+    input(b, n);
+    sort(all(a));
+    sort(all(b));
+    int i = 0, j = 0, cnt = 0;
+    while (i < n && j < m) {
+        if (a[i] > b[j] + k)
+        j++;
+        else if (a[i] < b[j] - k)
+        i++;
+        else {
+            cnt++;
+            i++;
+            j++;
+        }
+    }
+    print(cnt);
+    return 0;
+}
