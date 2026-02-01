75a720ba83e36841bd44fe986aa600792680e9f4
ShubhamKumarSahu-svg <shubhamisok@gmail.com>
2026-02-01 18:20:15 +0530

    Add CSES problem 1629: Movie Festival solution

---
+++ CSES_Problems/1629-Movie_Festival/solution.cpp
@@ -0,0 +1,104 @@
+// CSES Problem 1629: Movie Festival
+// Link: https://cses.fi/problemset/task/1629/
+// Description: In a movie festival n movies will be shown. You know the starting and ending tim...
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vc vector<char>
+#define vvc vector<vector<char>>
+#define pii pair<int, int>
+#define vpii vector<pair<int, int>>
+#define mii map<int, int>
+#define umii unordered_map<int, int>
+#define si set<int>
+#define msi multiset<int>
+
+#define printno cout << "NO" << endl;
+#define printyes cout << "YES" << endl;
+#define print(x) cout << (x) << endl;
+#define printsp(x) cout << (x) << " ";
+#define printarr(v)                                                            \
+for (auto &x : v)                                                            \
+cout << x << " ";
+#define printarrn(v)                                                           \
+for (auto &x : v)                                                            \
+cout << x << " ";                                                          \
+cout << endl;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define input(v, n)                                                            \
+for (int i = 0; i < n; i++)                                                  \
+cin >> v[i]
+#define input1(v, n)                                                           \
+for (int i = 1; i <= n; i++)                                                 \
+cin >> v[i]
+
+#define read1(a)                                                               \
+int a;                                                                       \
+cin >> a;
+#define read2(a, b)                                                            \
+int a, b;                                                                    \
+cin >> a >> b;
+#define read3(a, b, c)                                                         \
+int a, b, c;                                                                 \
+cin >> a >> b >> c;
+
+#define forp(i, n) for (int i = 0; i < n; i++)
+#define form(i, n) for (int i = n - 1; i >= 0; i--)
+
+int mex(vector<int> a) {
+    int n = a.size();
+    for (int i = 0; i < n; i++) {
+        while (a[i] >= 0 && a[i] < n && a[a[i]] != a[i]) {
+            swap(a[i], a[a[i]]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        if (a[i] != i)
+        return i;
+    }
+    return n;
+}
+
+const int mod = 1e9 + 7;
+const int inf = INT_MAX;
+const int m_inf = INT_MIN;
+
+void fastIO() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+void solve() {
+    read1(n);
+    vpii v(n);
+    forp(i, n) {
+        cin >> v[i].second;
+        cin >> v[i].first;
+    }
+    sort(all(v));
+    int cnt = 1;
+    int start_curr = v[0].second;
+    int end_curr = v[0].first;
+    for (int i = 1; i < n; i++) {
+        int start_next = v[i].second;
+        int end_next = v[i].first;
+        if (start_next >= end_curr) {
+            end_curr = end_next;
+            cnt++;
+        }
+    }
+    print(cnt);
+}
+
+signed main() {
+    fastIO();
+    solve();
+    return 0;
+}
